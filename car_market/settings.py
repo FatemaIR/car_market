a9f52866bea2a74a8f773cf8bc6e343e1eeeaaed
Fatemah AlRamzi <eng.fatemah.alramzi@gmail.com>
2018-12-10 19:20:11 +0300

    project_blog

--- car_market/settings.py
+++ car_market/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'mmj9k6e^e=si_+-lz-505sj(!)-!vly6zy3+xb(89^ldu&wcb9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '68.183.222.201'] #the first one is fixed for everyone (local host)
 
 
 # Application definition
@@ -77,10 +77,21 @@ WSGI_APPLICATION = 'car_market.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+#DATABASES = {
+ #   'default': {
+  #      'ENGINE': 'django.db.backends.sqlite3',
+   #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #}
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django',
+        'USER': 'django',
+        'PASSWORD': '6eb143ec5183bba5ac21b29c2e74db3d',
+        'HOST':'localhost',
+        'PORT':'',
     }
 }
 
